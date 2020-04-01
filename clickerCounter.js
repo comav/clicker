2b74e9bf0e09970d6bd811afca820619a36ac04c
comav <tsyomav@gmail.com>
2020-04-02 00:39:07 +0300

    Background can change now

--- clickerCounter.js
+++ clickerCounter.js
@@ -1,4 +1,20 @@
 function plusOneClick() {
     click = click + 1;
     document.getElementById("output").innerHTML = click;
+
+    if (click == 20) {
+        secondBackground();
+    }
+
+    if (click == 40) {
+        thirdBackground();
+    }
+
+    if (click == 60) {
+        fourthBackground();
+    }
+
+    if (click == 80) {
+        fifthBackground();
+    }
 }
