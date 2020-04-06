6c84c227e6eea996b4349e42d091ece7e51305a8
comav <tsyomav@gmail.com>
2020-04-06 19:13:38 +0300

    Beta mobile version

--- backgroundChanger.js
+++ backgroundChanger.js
@@ -1,6 +1,6 @@
 function secondBackground() {
     document.body.style.background = "url(background/bg2.jpg) no-repeat";
-    document.body.style.backgroundSize = "100%";
+    document.body.style.backgroundSize = "400%";
     document.getElementById('button').style.backgroundColor = "#3b4d42";
     document.getElementById('button').style.color = "#90a886"
     document.getElementById('text').style.color = "#90a886";
@@ -9,7 +9,7 @@ function secondBackground() {
 
 function thirdBackground() {
     document.body.style.background = "url(background/bg3.jpg) no-repeat";
-    document.body.style.backgroundSize = "100%";
+    document.body.style.backgroundSize = "400%";
     document.getElementById('button').style.backgroundColor = "#17221e";
     document.getElementById('button').style.color = "#b3bdbe"
     document.getElementById('text').style.color = "#b3bdbe";
@@ -18,7 +18,7 @@ function thirdBackground() {
 
 function fourthBackground() {
     document.body.style.background = "url(background/bg4.jpg) no-repeat";
-    document.body.style.backgroundSize = "100%";
+    document.body.style.backgroundSize = "400%";
     document.getElementById('button').style.backgroundColor = "#545518";
     document.getElementById('button').style.color = "#ffffff"
     document.getElementById('text').style.color = "#ffffff";
@@ -27,7 +27,7 @@ function fourthBackground() {
 
 function fifthBackground() {
     document.body.style.background = "url(background/bg5.jpg) no-repeat";
-    document.body.style.backgroundSize = "100%";
+    document.body.style.backgroundSize = "400%";
     document.getElementById('button').style.backgroundColor = "#d54e13";
     document.getElementById('button').style.color = "#f9d825"
     document.getElementById('text').style.color = "#f9d825";
